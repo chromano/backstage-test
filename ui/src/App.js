1586616eb043a3ccba14e5d5860a354b80201d53
Carlos H Romano <chromano@gmail.com>
2019-08-16 11:22:19 -0300

    Initial attempt to connect UI to the backend

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [request, setRequest] = useState('');
+  const [calculations, setCalculations] = useState([]);
+
+  const handleOnChange = event => {
+    setRequest(event.target.value);
+  }
+
+  const handleSubmit = event => {
+    if (request) {
+      fetch('/api/v1/calc/difference?n=' + request).then(response => {
+        setCalculations(calculations.then(result => result.unshift(response.json)));
+      });
+    }
+
+    setRequest('');
+    event.preventDefault();
+  };
+
+  useEffect(() => {
+    fetch('/api/v1/calc/difference').then(response => setCalculations(response.json()));
+  }, []);
+
   return (
     <div className="App container pt-5">
-      <form className="form-inline">
+      <form className="form-inline" onSubmit={handleSubmit}>
         <div className="form-group mx-sm-3 mb-2">
-          <input type="text" className="form-control" value="Enter the value"/>
+          <input type="text" className="form-control" value={request}
+            onChange={handleOnChange}
+            placeholder="Enter the value"/>
         </div>
         <button type="submit" className="btn btn-primary mb-2">Calculate</button>
       </form>
@@ -21,12 +45,14 @@ function App() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
+            {calculations.map && calculations.map(calculation => (
+              <tr key={calculation.id}>
+                <th scope="row">{calculation.datetime}</th>
+                <td>{calculation.number}</td>
+                <td>{calculation.solution}</td>
+                <td>{calculation.occurrences}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
